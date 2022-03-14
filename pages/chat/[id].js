ebb0e9961422cd676f175402750c1d11f349e225
Pedro Ferreira <pedroferrodude@hotmail.com>
2022-03-14 00:29:29 +0000

    started to set up server side rendering functionality

--- pages/chat/[id].js
+++ pages/chat/[id].js
@@ -2,8 +2,10 @@ import React from 'react';
 import styled from 'styled-components';
 import Head from 'next/head';
 import Sidebar from '../components/Sidebar';
+import ChatScreen from '../components/ChatScreen';
+import { db } from '../../firebase';
 
-const Chat = () => {
+const Chat = ({ chat, messages }) => {
   return (
     <Container>
       <Head>
@@ -19,8 +21,49 @@ const Chat = () => {
 
 export default Chat;
 
+export async function getServerSideProps(context) {
+  const ref = db.collection('chats').doc(context.query.id);
+  const messagesRes = await ref
+    .collection('messages')
+    .orderBy('timestamp', 'asc')
+    .get();
+
+  const messages = messagesRes.docs
+    .map((doc) => ({
+      id: doc.id,
+      ...doc.data()
+    }))
+    .map((message) => ({
+      ...message,
+      timestamp: message.timestamp.toDate().getTime()
+    }));
+
+  const chatRes = await ref.get();
+  const chat = {
+    id: chatRes.id,
+    ...chatRes.data()
+  };
+
+  return {
+    props: {
+      messages: JSON.stringify(messages),
+      chat: chat
+    }
+  };
+}
+
 const Container = styled.div`
   display: flex;
 `;
 
-const ChatContainer = styled.div``;
+const ChatContainer = styled.div`
+  flex: 1;
+  overflow: scroll;
+  height: 100vh;
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+`;
---
+++ pages/components/ChatScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ChatScreen = () => {
+  return (
+    <Container>
+      <h1>Chat Screen</h1>
+    </Container>
+  );
+};
+
+export default ChatScreen;
+
+const Container = styled.div``;
