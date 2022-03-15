b4c4ca2dd423a71ead6b97e77c8fda7a616142c6
pedro.ferreira@findmore.eu <pedro.ferreira@findmore.eu>
2022-03-15 14:59:57 +0000

    added messages to chat screen, added scroll functionality to side bar

--- pages/components/ChatScreen.js
+++ pages/components/ChatScreen.js
@@ -8,11 +8,16 @@ import { auth, db } from '../../firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useRouter } from 'next/router';
 import { useCollection } from 'react-firebase-hooks/firestore';
-import Message from './Message';
+import Message from './Message.js';
 import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
+import MicIcon from '@mui/icons-material/Mic';
+import { useState } from 'react';
+import firebase from 'firebase/compat/app';
+import getRecipientEmail from '../../utils/getRecipientEmail';
 
 const ChatScreen = ({ chat, messages }) => {
   const [user] = useAuthState(auth);
+  const [input, setInput] = useState('');
   const router = useRouter();
   const [messagesSnapshot] = useCollection(
     db
@@ -25,24 +30,53 @@ const ChatScreen = ({ chat, messages }) => {
   const showMessages = () => {
     if (messagesSnapshot) {
       return messagesSnapshot.docs.map((message) => {
-        <Message
-          key={message.id}
-          user={message.data.user}
-          message={{
-            ...message.data(),
-            timestamp: message.data().timestamp?.toDate().getTime()
-          }}
-        />;
+        return (
+          <Message
+            key={message.id}
+            user={message.data.user}
+            message={{
+              ...message.data(),
+              timestamp: message.data().timestamp?.toDate().getTime()
+            }}
+          />
+        );
+      });
+    } else {
+      return JSON.parse(messages).map((message) => {
+        return (
+          <Message key={message.id} user={message.user} message={message} />
+        );
       });
     }
   };
+
+  const handleSendMessage = (e) => {
+    e.preventDefault();
+
+    db.collection('users').doc(user.uid).set(
+      {
+        lastSeen: firebase.firestore.FieldValue.serverTimestamp()
+      },
+      { merge: true }
+    );
+    db.collection('chats').doc(router.query.id).collection('messages').add({
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      message: input,
+      user: user.email,
+      photoURL: user.photoURL
+    });
+
+    setInput('');
+  };
+
+  const recipientEmail = getRecipientEmail(chat.users, user);
 
   return (
     <Container>
       <Header>
         <Avatar />
         <HeaderInfo>
-          <h3>Recipient Email</h3>
+          <h3>{recipientEmail}</h3>
           <p>Last Seen..</p>
         </HeaderInfo>
         <HeaderIcons>
@@ -57,12 +91,22 @@ const ChatScreen = ({ chat, messages }) => {
 
       <MessageContainer>
         {showMessages()}
+
         <EndOfMessage />
       </MessageContainer>
 
       <InputContainer>
         <InsertEmoticonIcon />
-        <Input />
+        <Input value={input} onChange={(e) => setInput(e.target.value)} />
+        <button
+          hidden
+          disabled={!input}
+          type="submit"
+          onClick={handleSendMessage}
+        >
+          Send Message
+        </button>
+        <MicIcon />
       </InputContainer>
     </Container>
   );
@@ -101,11 +145,26 @@ const HeaderInfo = styled.div`
 
 const HeaderIcons = styled.div``;
 
-const MessageContainer = styled.div``;
+const MessageContainer = styled.div`
+  padding: 30px;
+  background-color: lightgrey;
+  min-height: 90vh;
+`;
 
 const EndOfMessage = styled.div``;
 
-const Input = styled.div``;
+const Input = styled.input`
+  flex: 1;
+  outline: 0;
+  border: none;
+  align-items: center;
+  padding: 20px;
+  margin: 0 15px;
+  border-radius: 10px;
+  position: sticky;
+  bottom: 0;
+  background-color: whitesmoke;
+`;
 
 const InputContainer = styled.form`
   display: flex;
